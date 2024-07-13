613ef9d9e57e108b60e9e79e955711c496717448
Rusherab <124442761+Rusherab@users.noreply.github.com>
2024-07-13 20:53:27 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Index() {
           RushBot Destek
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with MusicMaker? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+      RushBot'un Web Sitesine Hoş Geldiniz. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
